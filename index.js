36db757e6a498cec5f2b15fec162c37313a33489
SheikhZayed <cyber2maro@gmail.com>
2015-05-27 14:35:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 var express = require('express');
-var bodyParser = require('body-parser');
 var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(request, response) {
+  response.send('Hello World!');
+});
+
+var bodyParser = require('body-parser');
 
 var WEBHOOK_SECRET = "62DZWMCCFFHTTQ44CG3WUQ94CTT7GAAN";
 
 app.post('/telerivet/webhook', 
   bodyParser.urlencoded({ extended: true }),
   function(req, res) {
-      res.send('hello');
       var secret = req.body.secret;
       if (secret !== WEBHOOK_SECRET) {
           res.status(403).end();
@@ -33,4 +40,6 @@ app.post('/telerivet/webhook',
   }
 );
 
-app.listen(process.env.PORT || 5000);
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
