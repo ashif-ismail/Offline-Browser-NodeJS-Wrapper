d116fadf375429f14d03a5ca794dddfee320f8ec
SheikhZayed <cyber2maro@gmail.com>
2015-05-28 15:50:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
 var express = require('express');
+var request = require("request");
 var app = express();
-
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
-
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.send('Hello Cruel World!');
 });
-
 var bodyParser = require('body-parser');
-
 var WEBHOOK_SECRET = "62DZWMCCFFHTTQ44CG3WUQ94CTT7GAAN";
-
 app.post('/telerivet/webhook', 
   bodyParser.urlencoded({ extended: true }),
   function(req, res) {
@@ -20,26 +16,23 @@ app.post('/telerivet/webhook',
           res.status(403).end();
           return;
       }
-      
       if (req.body.event == 'incoming_message') {
-      
         var content = req.body.content;
         var from_number = req.body.from_number;
-        var phone_id = req.body.phone_id;
-        
+	    var phone_id = req.body.phone_id;
+	  }
+    	request("http://boilerpipe-web.appspot.com/extract?url=http://www.caclub.in&extractor=LargestContentExtractor&output=text&extractImages=", function(error, response, data) {
         // do something with the message, e.g. send an autoreply
         res.json({
-          messages: [
-            { content: "Thanks for your message!,Our Backend Is Still In Alpha Stage,Hang Tight" }
+			 messages: [
+            { content: ""+data}
           ]
-        });
-        
-      }  
-      
-      res.status(200).end();
+		});
+       res.status(200).end();
+		 });
   }
-);
-
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
+	);  
+	  app.listen(app.get('port'), function() {
+     console.log('Node app is running on port', app.get('port'));
+     });
+ 
